1f7bcf48ac5256ff0c8724d5be76a93adb812a17
Pedro Henrique Guimarães Rocha Rolin <pedrorocha2010@hotmail.com>
2024-12-13 23:01:02 -0300

    js

--- teste camisetas/script.js
+++ teste camisetas/script.js
@@ -1,1 +1,7 @@
-let shirt = document.querySelector('li')
+function search() {
+    let input = document.querySelector('input#searchbar').value
+    input = input.toLowerCase()
+    let shirt = document.querySelector('article.shirt')
+
+    for (i = 0; i < shirt.length; i++)
+}
